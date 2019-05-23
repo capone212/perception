78b3689181e2d47f5b113f70078a9f8c1a55f12b
Anzor Apshev <anzor.apshev@ru.axxonsoft.com>
2019-05-24 01:17:11 +0300

    Added some implementation to the last hw

--- RoboticsPerceptionWeek4AssignmentCode/LinearPnP.m
+++ RoboticsPerceptionWeek4AssignmentCode/LinearPnP.m
@@ -13,10 +13,31 @@ function [C, R] = LinearPnP(X, x, K)
 % for the P = [R t] matrix then use the K matrix to correct the error, this is
 % more numeically unstable, and thus it is better to calibrate the x values
 % before the computation of P then extract R and t directly
+AA = [];
+invK = inv(K);
 
+for i = 1:size(X, 1)
+    xk = invK *  [ x(i, :) 1]';
+    u = xk(1);
+    v = xk(2);
+    Xt = [X(i,:) 1];
+    A = [zeros(1,4) -Xt         v*Xt;
+         Xt         zeros(1,4)  -u*Xt;
+         -v*Xt      u*Xt        zeros(1,4)];
+    AA = [AA; A];
+end
+% Solve system of linear equations using SVD
+[~, ~, V] = svd(AA);
+P = reshape(V(:, end), 3, 4)
+[U, D, V] = svd(P(:, 1:3));
+R = U*V';
+C = P(:,4) / D(1,1);
 
+if (det(U*V') < 0)
+    R = -R;
+    C = -C;
+end
 
-
-
+det(R)
 
 
--- RoboticsPerceptionWeek4AssignmentCode/LinearTriangulation.m
+++ RoboticsPerceptionWeek4AssignmentCode/LinearTriangulation.m
@@ -13,6 +13,18 @@ function X = LinearTriangulation(K, C1, R1, C2, R2, x1, x2)
 % Outputs: 
 %     X - size (N x 3) matrix whos rows represent the 3D triangulated
 %       points
-
-
+P1 = K * [R1 C1];
+P2 = K * [R2 C2];
+X = [];
+% for each here
+for i = 1:size(x1,1)
+    cx1 = [x1(i, :) 1]';
+    cx2 = [x2(i, :) 1]';
+    skew1 = Vec2Skew(cx1);
+    skew2 = Vec2Skew(cx2);
+    A=[skew1 * P1; skew2 * P2];
+    [u, d, v] = svd(A);
+    XR = v(:,end)/v(end, end);
+    X = [X; XR(1:3)'];
+end
 
